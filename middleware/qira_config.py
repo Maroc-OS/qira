7ea9caa2cf2abe701953c1a3f5ccaf15bd0a1dd6
Yahya Lmallas <merruk.company@gmail.com>
2016-05-05 13:59:12 +0200

    Same series, Adding Comments, DOT temp files, and moving static2 path
    Add QIRA dirs
    Add DOT temp files.
    Add Graph temp file.
    Add QIRA bin/asm temp files.
    Use 'os.path.join' to support multi OS's without conflicts.
    Moving static2 path append from two files to the config file.
    
    PyLint Score :
    
    
    Global evaluation
    -----------------
    Your code has been rated at 9.74/10 (previous run: 9.55/10, +0.19)

--- middleware/qira_config.py
+++ middleware/qira_config.py
@@ -2,30 +2,56 @@ import os
 import sys
 
 
+# Trace Libraries
 TRACE_LIBRARIES = False
+
+# Web Server
 HOST = '0.0.0.0'
 WEB_PORT = 3002
 SOCAT_PORT = 4000
 FORK_PORT = SOCAT_PORT + 1
+
+# Use PIN
 USE_PIN = False
 
+# Temp Directories
 if os.name == "nt":
     LOG_FILE_BASE = "c:/qiratmp/qira_log"
+    ASM_FILE_BASE = "c:/qiratmp/qira_asm"
     TRACE_FILE_BASE = "c:/qiratmp/qira_logs/"
+    GRAPH_FILE_BASE = "c:/qiratmp/qira_graph.png"
+    BINARY_FILE_BASE = "c:/qiratmp/qira_binary"
+    IN_DOT_FILE_BASE = "c:/qiratmp/qira_in.dot"
+    OUT_DOT_FILE_BASE = "c:/qiratmp/qira_out.dot"
 else:
     LOG_FILE_BASE = "/tmp/qira_log"
+    ASM_FILE_BASE = "/tmp/qira_asm"
     TRACE_FILE_BASE = "/tmp/qira_logs/"
+    GRAPH_FILE_BASE = "/tmp/qira_graph.png"
+    BINARY_FILE_BASE = "/tmp/qira_binary"
+    IN_DOT_FILE_BASE = "/tmp/qira_in.dot"
+    OUT_DOT_FILE_BASE = "/tmp/qira_out.dot"
 
 # Specify QIRA BASEDIR.
-BASEDIR = os.path.realpath(
-    os.path.dirname(os.path.realpath(__file__))+"/../")
-
+FILEDIR = os.path.realpath(
+    os.path.dirname(os.path.realpath(__file__)))
+BASEDIR = os.path.join(FILEDIR, "..")
 sys.path.append(BASEDIR)
+
+# New home of static2
+sys.path.append(os.path.join(BASEDIR, "static2"))
+
+# QIRA Directories
+LIB_DIR = os.path.join(BASEDIR, "libs")
+PIN_DIR = os.path.join(BASEDIR, "tracers", "pin")
+QEMU_DIR = os.path.join(BASEDIR, "tracers", "qemu")
+PIN_BINARY = os.path.join(PIN_DIR, "pin-latest", "pin")
 
 # BAP is no longer supported
 WITH_BAP = False
 
 # TODO: Make this True in v3
+# Static Engine
 WITH_STATIC = False
 STATIC_ENGINE = "builtin"
 
