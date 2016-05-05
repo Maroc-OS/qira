e5af99c520e9f787c1606b6b93c7e3e329d35b30
Yahya Lmallas <merruk.company@gmail.com>
2016-05-05 03:00:09 +0200

    Refactoring qira_config, make it python standard, and fixes for windows
    This Commit makes 'qira_config.py' a standard python compatible file, and add some windows fixes, next commits will follow, this changes.
    
    This new series of changes will update *QIRA* python files to meet the standards and minor fixes, and memory optimization.
    
    PyLint Score :
    
    Global evaluation
    -----------------
    Your code has been rated at 9.55/10 (previous run: 9.55/10, +0.00)

--- middleware/qira_config.py
+++ middleware/qira_config.py
@@ -1,4 +1,6 @@
-import os,sys
+import os
+import sys
+
 
 TRACE_LIBRARIES = False
 HOST = '0.0.0.0'
@@ -6,21 +8,31 @@ WEB_PORT = 3002
 SOCAT_PORT = 4000
 FORK_PORT = SOCAT_PORT + 1
 USE_PIN = False
-if os.name == "nt":
-  TRACE_FILE_BASE = "c:/qiratmp"
-else:
-  TRACE_FILE_BASE = "/tmp/qira_logs/"
 
-BASEDIR = os.path.realpath(os.path.dirname(os.path.realpath(__file__))+"/../")
+if os.name == "nt":
+    LOG_FILE_BASE = "c:/qiratmp/qira_log"
+    TRACE_FILE_BASE = "c:/qiratmp/qira_logs/"
+else:
+    LOG_FILE_BASE = "/tmp/qira_log"
+    TRACE_FILE_BASE = "/tmp/qira_logs/"
+
+# Specify QIRA BASEDIR.
+BASEDIR = os.path.realpath(
+    os.path.dirname(os.path.realpath(__file__))+"/../")
+
 sys.path.append(BASEDIR)
 
 # BAP is no longer supported
 WITH_BAP = False
 
-# TODO: make this true in v3
+# TODO: Make this True in v3
 WITH_STATIC = False
 STATIC_ENGINE = "builtin"
-STATIC_CACHE_BASE = "/tmp/qira_static_cache/"
 
+if os.name == "nt":
+    STATIC_CACHE_BASE = "c:/qiratmp/qira_static_cache/"
+else:
+    STATIC_CACHE_BASE = "/tmp/qira_static_cache/"
+
+# Enable WebSocket Debugging
 WEBSOCKET_DEBUG = False
-
